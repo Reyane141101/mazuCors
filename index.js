a6af151abf9c43cdc5a9eb14189002f2bab9e77c
Marine Thunet <marine.thunet@epita.fr>
2023-06-22 03:22:43 +0200

    final

--- index.js
+++ index.js
@@ -6,22 +6,52 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Route pour l'endpoint de votre application web
 app.post('/api/predict', async (req, res) => {
   try {
-    const inputData = req.body; // Données envoyées par votre application web
-    const mlEndpoint = 'http://0e716dec-8f88-44b1-b174-a19500038284.westeurope.azurecontainer.io/score'; // URL de votre endpoint ML
-
-    // Effectuer une requête HTTP vers votre endpoint ML
-    const response = await axios.post(mlEndpoint, inputData, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Autorization': 'Bearer vUmrMS6DAX0VlGlcpRxqDQz5CEET77lw'
+    // Données d'exemple statiques
+    const inputData = {
+      "Inputs": {
+        "input1": [
+          {
+            "Vegetable": "potato",
+            "Season": "winter",
+            "Month": "jan",
+            "Temp": 17,
+            "Deasaster Happen in last 3month": "no",
+            "Vegetable condition": "fresh"
+          },
+          {
+            "Vegetable": "tomato",
+            "Season": "winter",
+            "Month": "jan",
+            "Temp": 17,
+            "Deasaster Happen in last 3month": "no",
+            "Vegetable condition": "fresh"
+          },
+          {
+            "Vegetable": "peas",
+            "Season": "winter",
+            "Month": "jan",
+            "Temp": 17,
+            "Deasaster Happen in last 3month": "no",
+            "Vegetable condition": "fresh"
+          }
+        ]
       },
+      "GlobalParameters": {}
+    };
+
+    const mlEndpoint = 'http://0e716dec-8f88-44b1-b174-a19500038284.westeurope.azurecontainer.io/score';
+    const mlHeaders = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer vUmrMS6DAX0VlGlcpRxqDQz5CEET77lw'
+    };
+
+    const mlResponse = await axios.post(mlEndpoint, inputData, {
+      headers: mlHeaders
     });
 
-    // Renvoyer les résultats au client (votre application web)
-    res.json("HELLO ZEBOOOOOOOOO");
+    res.json(mlResponse.data);
   } catch (error) {
     console.error('Erreur lors de l\'appel à l\'endpoint ML:', error);
     res.status(500).json({ error: 'Erreur lors de l\'appel à l\'endpoint ML' });
